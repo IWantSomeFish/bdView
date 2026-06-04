af330a6e71bb1f82df1bf558fc1a213845bbcdbe
avinogrado <vino74974@gmail.com>
2026-06-04 13:35:25 +0300

    server is starting

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
+import path from "path";
 import { app } from "./app.js";
 import dotenv from "dotenv";
 
-dotenv.config({ path: '../../.env' });
+dotenv.config({ path: path.resolve(process.cwd(), "../.env") });
 
-const PORT = process.env.PORT;
+const PORT = process.env.BACKEND_PORT;
 
 app.listen(PORT, () => {
     console.log(
