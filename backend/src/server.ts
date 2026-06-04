c8035156e04ef4f1c3b1ab2aaa5dcaee58e192af
avinogrado <vino74974@gmail.com>
2026-06-04 13:30:42 +0300

    еще немного конфигурирования

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: '../../.env' });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(
