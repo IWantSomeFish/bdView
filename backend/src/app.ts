734047adbd689cdbef9a7ed5ae80cd9efd254aac
avinogrado <vino74974@gmail.com>
2026-06-05 10:28:17 +0300

    cors for frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,14 @@ import express from "express";
 
 import parseRoutes from "./routes/api.routes.js";
 import { loggerMiddleware } from "./middlewares/logger.middleware.js";
+import cors from "cors";
+import { getEnvVariable } from "./utils/env.helper.js";
 
 export const app = express();
 
 app.use(express.json());
 app.use(loggerMiddleware);
-
+app.use(cors({
+    origin: `http://localhost:${getEnvVariable("FRONTEND_PORT")}`,
+}))
 app.use("/api", parseRoutes);
