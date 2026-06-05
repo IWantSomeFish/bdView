7aafea0e4a9003199a06594d43c13310128522ff
avinogrado <vino74974@gmail.com>
2026-06-05 10:05:31 +0300

    logger middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import parseRoutes from "./routes/api.routes.js";
+import { loggerMiddleware } from "./middlewares/logger.middleware.js";
 
 export const app = express();
 
 app.use(express.json());
+app.use(loggerMiddleware);
 
 app.use("/api", parseRoutes);
