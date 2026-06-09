7962736a5128e86ceaceb4fb81ae68ccf4c4c8e8
avinogrado <vino74974@gmail.com>
2026-06-09 11:19:52 +0300

    объединение таблиц

--- backend/src/services/api.service.ts
+++ backend/src/services/api.service.ts
@@ -8,11 +8,49 @@ export class ParseService {
 
     async parse(buffer: Buffer) {
         const raw = await this.repo.dump(buffer);
-        const result: Record<string, unknown> = {};
+        const tables: Record<string, unknown> = {};
+        
         for (const table of REQUIRED_TABLES) {
-            result[table] = raw[table] ?? [];
+            tables[table] = raw[table] ?? [];
         }
-        // тут потом появится нормализация DTO
-        return result;
+        const routes = tables.routes as any[];
+        const routeSegments = tables.route_segments as any[];
+        const wifiFingerprints = tables.wifi_fingerprints as any[];
+
+        const segmentsWithWifi = connectTables(
+            routeSegments,
+            wifiFingerprints,
+            "segmentId",
+            "segmentId",
+            "wifiFingerprints",
+        );
+
+        const routesWithSegments = connectTables(
+            routes,
+            segmentsWithWifi,
+            "routeId",
+            "routeId",
+            "routeSegments",
+        );
+    console.log(routesWithSegments[1].routeSegments)
+    return routesWithSegments;
     }
+}
+
+function connectTables<TParent extends Record<string, any>, TChild extends Record<string, any>
+    >(parents: TParent[],children: TChild[], parentKey: keyof TParent, childKey: keyof TChild,childField: string) {
+    const childrenMap = new Map<any,TChild[]>();
+    
+    for (const child of children) {
+        const key = child[childKey];
+        const list = childrenMap.get(key) ?? [];
+        list.push(child);
+
+        childrenMap.set(key,list);
+    }
+
+    return parents.map(parent => ({
+        ...parent,
+        [childField]: childrenMap.get(parent[parentKey]) ?? []
+    }))
 }
