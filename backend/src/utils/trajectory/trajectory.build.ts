2525fa1373fb23fabfc2c5e45b686c4f08cc7ad5
avinogrado <vino74974@gmail.com>
2026-06-10 15:18:39 +0300

    GPS to H3 points coversion

---
+++ backend/src/utils/trajectory/trajectory.build.ts
@@ -0,0 +1,38 @@
+import { latLngToCell } from "h3-js";
+import { H3Trajectory } from "./trajectory.types";
+
+export function runToH3Trajectory(run: Record<string, any>,resolution = 11): H3Trajectory | null {
+
+    if (run.source !== "MANUAL") {
+        return null;
+    }
+
+    const points = run["snapshotPoints"];
+
+    if (!points || points.length === 0) {
+        return null;
+    }
+
+    // 1. сортировка по времени
+    const sorted = [...points].sort(
+        (a, b) => a.gpsTimestamp - b.gpsTimestamp,
+    );
+
+    // 2. H3 conversion
+    const h3Sequence = sorted.map((p) =>
+        latLngToCell(p.gpsLatitude, p.gpsLongitude, resolution),
+    );
+
+    // 3. дедуп подряд идущих ячеек
+    const compressed: string[] = [];
+
+    for (const cell of h3Sequence) {
+        const last = compressed[compressed.length - 1];
+
+        if (last !== cell) {
+            compressed.push(cell);
+        }
+    }
+    const result: H3Trajectory = {runId: run.runId, routeId: run.routeId,segmentId: run.segmentId,points: compressed}
+    return result;
+}
