8ffbdcb3d124a6796f4e3bd61d512cccc61d4d34
avinogrado <vino74974@gmail.com>
2026-06-16 13:14:00 +0300

    вынос экстракта в хелпер

--- backend/src/services/api.service.ts
+++ backend/src/services/api.service.ts
@@ -1,3 +1,4 @@
+import { extractCalibrations } from "../utils/helpers.extractCalibs";
 import { saveJSON } from "../utils/helpers.saveJSON";
 import { runToH3Trajectory } from "../utils/trajectory/trajectory.build";
 import { DatasetBuilder } from "../utils/trajectory/trajectory.datasetBuilder";
@@ -25,28 +26,4 @@ export class mainService {
         const dataSet: ModelSample[] = this.builder.buildBatch(tokenizedTrajectories)
         saveJSON(dataSet)
     }
-}
-
-export function extractCalibrations(routes: any[]): H3Trajectory[] {
-    const runs: H3Trajectory[] = [];
-
-    for (const route of routes) {
-        const routeSegments = route.routeSegments ?? [];
-
-        for (const segment of routeSegments) {
-            const calibrationRuns = segment.calibrations ?? [];
-
-            for (const run of calibrationRuns) {
-                if (run.source === "MANUAL")
-                    runs.push({
-                    runId: run.runId,
-                    routeId: run.routeId,
-                    segmentId: run.segmentId,
-                    points: run.snapshotPoints
-                });
-            }
-        }
-    }
-
-    return runs; 
 }
