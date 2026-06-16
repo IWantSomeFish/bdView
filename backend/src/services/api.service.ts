46bed568ba97dd2696689fe230e1e4fa2c9125f6
avinogrado <vino74974@gmail.com>
2026-06-16 11:19:20 +0300

    выделение главного сервиса

--- backend/src/services/api.service.ts
+++ backend/src/services/api.service.ts
@@ -1,63 +1,11 @@
-import { SqliteRepository } from "../repositories/sqlite.repository";
-import { REQUIRED_TABLES } from "../types/api.types";
+import { ParseService } from "./parse.service";
 
-export class ParseService {
-    constructor(
-        private readonly repo = new SqliteRepository(),
-    ) { }
+export class mainService {
+        constructor(
+            private readonly parser: ParseService = new ParseService
+        ) {}
 
-    async parse(buffer: Buffer) {
-        const raw = await this.repo.dump(buffer);
-        const tables: Record<string, unknown> = {};
-
-        for (const table of REQUIRED_TABLES) {
-            tables[table] = raw[table] ?? [];
-        }
-        const routes = tables.routes as any[];
-        const routeSegments = tables.route_segments as any[];
-        const calibrations = tables.calibration_runs as any[];
-        const snapshots = tables.raw_calibration_snapshots as any[];
-
-        const calibrationsWithSnapshots = connectTables(
-            calibrations,
-            snapshots,
-            "runId",
-            "calibrationRunId",
-            "snapshotPoints"
-        )
-        const segmentsCalibrations = connectTables(
-            routeSegments,
-            calibrationsWithSnapshots,
-            "segmentId",
-            "segmentId",
-            "calibrations",
-        );
-
-        const routesWithSegments = connectTables(
-            routes,
-            segmentsCalibrations,
-            "routeId",
-            "routeId",
-            "routeSegments",
-        );
-        return routesWithSegments;
+    async getRoutes(buffer: Buffer) {
+        return this.parser.parse(buffer)
     }
-}
-
-function connectTables<TParent extends Record<string, any>, TChild extends Record<string, any>
->(parents: TParent[], children: TChild[], parentKey: keyof TParent, childKey: keyof TChild, childField: string) {
-    const childrenMap = new Map<any, TChild[]>();
-
-    for (const child of children) {
-        const key = child[childKey];
-        const list = childrenMap.get(key) ?? [];
-        list.push(child);
-
-        childrenMap.set(key, list);
-    }
-
-    return parents.map(parent => ({
-        ...parent,
-        [childField]: childrenMap.get(parent[parentKey]) ?? []
-    }))
 }
