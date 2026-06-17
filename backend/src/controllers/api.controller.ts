8e2f6cd6831abb512c175a3e2c8eb06ad4dbe0db
avinogrado <vino74974@gmail.com>
2026-06-17 16:52:50 +0300

    морж конфликты

--- backend/src/controllers/api.controller.ts
+++ backend/src/controllers/api.controller.ts
@@ -22,15 +22,6 @@ export class ApiController {
                     error: "database file required",
                 });
             }
-<<<<<<< HEAD
-            const result = await service.getRoutes(req.file.buffer);
-||||||| 334d398
-            const result = await service.parse(req.file.buffer);
-            trainModel(extractCalibrations(result))
-            if (result instanceof Error) {
-                return res.status(400).json({error: `${result}`})
-            }
-=======
             const result = await service.getRoutes(req.file.buffer);
             return res.json(result);
         }
@@ -45,7 +36,6 @@ export class ApiController {
             }
             const parsedDB = await service.getRoutes(req.file.buffer);
             const result = await service.getSimilar(parsedDB,req.file.buffer);
->>>>>>> origin/master
             return res.json(result);
         }
     }
