984c653c7afbf83346d143157e18d53a99095207
gleb <smirnovg2005@gmail.com>
2026-06-18 11:03:36 +0300

    oops

--- frontend/frontend/src/domain/IDatabaseRepository.ts
+++
@@ -1,7 +0,0 @@
-import type { ParseResult, SimilarResult } from './types';
-
-export interface IDatabaseRepository {
-  checkHealth(): Promise<void>;
-  uploadDatabase(file: File): Promise<ParseResult>;
-  uploadSimilar(file: File): Promise<SimilarResult>;
-}
--- frontend/frontend/src/presentation/pages/UploadPage.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import FileUpload from '../components/FileUpload';
-
-const UploadPage: React.FC = () => <FileUpload />;
-
-export default UploadPage;
